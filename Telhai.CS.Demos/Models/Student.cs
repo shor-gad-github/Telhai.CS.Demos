dcca05dbc0eb5c18ce54c48d7debb64baf9d086e
shor-gad-github <shorgad@outlook.com>
2022-11-29 11:25:42 +0200

    Add Student Class

---
+++ Telhai.CS.Demos/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telhai.CS.Demos.Models
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public int Age { get; set; }
+
+
+        public Student()
+        {
+        }
+
+        public Student(string name,int age)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+
+
+    }
+}
