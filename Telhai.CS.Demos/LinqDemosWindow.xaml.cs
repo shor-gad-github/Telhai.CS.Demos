0a6c0f2ec74217fbe0e503159fa04c4f91d52d60
shor-gad-github <shorgad@outlook.com>
2022-12-27 11:36:57 +0200

    Func

--- Telhai.CS.Demos/LinqDemosWindow.xaml.cs
+++ Telhai.CS.Demos/LinqDemosWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,9 +25,31 @@ namespace Telhai.CS.Demos
             InitializeComponent();
         }
 
+        public bool MyFunc(int n)
+        {
+            return n < 5;
+        }
+
         private void Demo1_Click(object sender, RoutedEventArgs e)
         {
+            int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0, 10 };
+            //Query Syntav
+            var data = from num in numbers
+                       where num < 5
+                       select (num);
 
+            //Lambda function Syntax
+            var dataFunc = numbers.Where(n=>n<5).Select(n => n * 2);
+
+
+            //Lambda function Syntax
+            var dataFuncRef = numbers.Where(MyFunc).Select(n => n*2);
+
+
+            foreach (var item in dataFunc)
+            {
+                ResultTxtBox.Text += "[" + item.ToString() + "] ";
+            }
         }
 
         private void Demo2_Click(object sender, RoutedEventArgs e)
