674cbf110bc3d1457dff0fa6ce816c69f05daa33
shor-gad-github <shorgad@outlook.com>
2023-01-03 11:41:43 +0200

    API

---
+++ Telhai.CS.ServerAPI/Repositories/IStudentsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telhai.CS.Demos.Models
+{
+    public interface IStudentsRepository
+    {
+        void AddStudent(Student student);
+        void UpdateStudent(Student student);
+        void RemoveStudent(string id);
+        Student[] Students { get;  }
+
+    }
+}
---
+++ Telhai.CS.ServerAPI/Repositories/StudentsRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telhai.CS.Demos.Models
+{
+    public class StudentsRepository : IStudentsRepository
+    {
+        private List<Student> _students;
+        //02 Static Member private
+        static private StudentsRepository _instance = null;
+
+        //01 change to private
+        private StudentsRepository()
+        {
+            _students = new List<Student>();
+        }
+
+        //03 Get Factory Of StudentsRepository  as singelton
+        public static StudentsRepository Instance
+        {
+            get
+            { 
+              if (_instance == null)
+                {
+                    _instance = new StudentsRepository();
+
+                }
+                return _instance;
+            }
+        }
+
+
+
+
+        public Student[] Students
+        {
+            get { return _students.ToArray(); }
+
+        }
+
+        public void AddStudent(Student student)
+        {
+            this._students.Add(student);
+        }
+
+        public void UpdateStudent(Student student)
+        {
+            int indexFound = this._students.FindIndex(s => s.Id == student.Id);
+            if (indexFound >= 0)
+            {
+                this._students[indexFound] = student;
+
+            }
+
+
+        }
+
+        public void RemoveStudent(string id)
+        {
+            int indexFound = this._students.FindIndex(s => s.Id == id);
+            if (indexFound >= 0)
+            {
+                this._students.RemoveAt(indexFound);
+            }
+        }
+
+    }
+}
