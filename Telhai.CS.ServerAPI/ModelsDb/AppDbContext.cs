beb14334e33be26275f1b607a2c78681b6513f0f
shor-gad-github <shorgad@outlook.com>
2023-01-24 08:00:34 +0200

    DbContext

---
+++ Telhai.CS.ServerAPI/ModelsDb/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Telhai.CS.ServerAPI.ModelsDb
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext()
+        {
+             
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Test");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectModels; AttachDbFilename='D:\Courses\.Net_Core_C#\01_.NetCore_Overview\ClassWork\Telhai.CS.Demos\DB\DbTesting.mdf'");
+       
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+        //entities
+        public DbSet<Log> Logs { get; set; }
+
+
+
+
+    }
+}
